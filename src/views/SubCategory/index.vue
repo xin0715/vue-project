7bf5b0a5bf4589ef243087947a29450d5851196a
amy <a0911405828@gmail.com>
2024-04-28 00:19:16 +0800

    列表無限載入實現

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -23,12 +23,29 @@ const reqData = ref({
   
 const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value)
-  console.log(res)
   goodList.value = res.result.items
 }
   
 onMounted(() => getGoodList())
 
+const tabChange =()=>{
+  reqData.value.page =1
+  getGoodList()
+}
+
+const disabled = ref(false)
+const load =async()=>{
+  console.log("more load");
+  reqData.value.page ++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value,...res.result.items]
+
+  if(reqData.value.items.length === 0){
+    disabled.value = true
+  }
+
+
+}
 </script>
 
 <template>
@@ -43,12 +60,12 @@ onMounted(() => getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load">
          <!-- 商品列表-->
          <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"></GoodsItem>
       </div>
