b5e0a11a731f7c7e7defe6647dbcac78f9a6e2d2
amy <a0911405828@gmail.com>
2024-04-29 23:28:07 +0800

    組件圖片放大鏡大圖顯示及優化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -16,12 +16,15 @@ const enterhandler =(i)=>{
     activeIndex.value= i
 }
 
+const positionX = ref(0)
+const positionY = ref(0)
 const left = ref(0)
 const top = ref(0)
 const target = ref(null)
 const {elementX,elementY,isOutside} = useMouseInElement(target)
 
-watch([elementX,elementY],()=>{
+watch([elementX,elementY,isOutside],()=>{
+    if(isOutside.value)return
     if(elementX.value>100&& elementX.value<300){
         left.value = elementX.value -100
     }
@@ -34,6 +37,9 @@ watch([elementX,elementY],()=>{
     if(elementY.value>300){top.value =200}
     if(elementY.value<100){top.value =0}
 
+    positionX.value =-left.value *2
+    positionY.value =-top.value *2
+
 })
 </script>
 
@@ -44,7 +50,7 @@ watch([elementX,elementY],()=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -56,10 +62,10 @@ watch([elementX,elementY],()=>{
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
