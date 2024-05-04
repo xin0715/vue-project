e34b33dcceb202760561a0f29883bf3cf2794ec7
amy <a0911405828@gmail.com>
2024-05-05 01:32:08 +0800

    登入pinia數據持久化

--- src/main.js
+++ src/main.js
@@ -10,11 +10,13 @@ import '@/styles/common.scss'
 
 import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -14,4 +14,7 @@ export const useUserStore = defineStore('user', () => {
         UserInfo,
         getUserInfo
     }
-})
+},
+    {
+        persist: true,
+    })
