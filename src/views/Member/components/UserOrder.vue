8b2205ea57eb9f8c6e5b7e90f940e82651c32b49
amy <a0911405828@gmail.com>
2024-05-19 17:28:58 +0800

    會員中心訂單基礎參數獲取列表

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -9,8 +9,24 @@ const tabTypes = [
   { name: "complete", label: "已完成" },
   { name: "cancel", label: "已取消" }
 ]
-// 订单列表
-const orderList = []
+// // 订单列表
+// const orderList = []
+import { getUserOrder } from '@/apis/order'
+import { onMounted, ref } from 'vue'
+
+// 获取订单列表
+const orderList = ref([])
+const params = ref({
+  orderState: 0,
+  page: 1,
+  pageSize: 2
+})
+const getOrderList = async () => {
+  const res = await getUserOrder(params.value)
+  orderList.value = res.result.items
+//   total.value = res.result.counts
+}
+onMounted(() => getOrderList())
 
 </script>
 
